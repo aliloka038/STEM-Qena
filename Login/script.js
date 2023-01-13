af059c9950c02b3c9974e148f3a30798f019dc54
aliloka038 <114072461+aliloka038@users.noreply.github.com>
2023-01-13 19:04:43 +0200

    Add files via upload

---
+++ Login/script.js
@@ -0,0 +1,11 @@
+    function OpenOrClose() {
+      document.getElementById("mySidebar").classList.toggle("open");
+      document.getElementById("main").classList.toggle("open");
+      }
+    
+      setTimeout(function() {
+        document.querySelector('.loader-container').style.display = 'none';
+        document.querySelector('.main-content').classList.add('visible');
+        document.body.style.overflow = "auto"
+        }, 2000);
+    
